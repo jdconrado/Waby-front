91465e50b9be2a2dab477365a8b4c8478314ec3f
BrayanU <buseche@uninorte.edu.co>
2020-04-20 02:02:09 -0500

    order

--- src/components/orders.js
+++ src/components/orders.js
@@ -187,6 +187,24 @@ export default function Orders() {
     obj.estado = status;
     var js = JSON.stringify(obj);
     setOpen(false);
+    var url = 'http://localhost:8000/pedidos/crear';
+    postData(url, js);
+  }
+  async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer',
+      body: data // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
   }
 
   const handleChange = (evt, type, i) => {
