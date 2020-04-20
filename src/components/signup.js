e00e62e7afb293251758b7ec9ba574780421a230
Moisesdd <moisesdd@uninorte.edu.co>
2020-04-20 02:21:04 -0500

    Post de sign up

--- src/components/signup.js
+++ src/components/signup.js
@@ -113,8 +113,28 @@ export default function SignUn() {
     obj.password = password;
     console.log(obj);
     var js = JSON.stringify(obj);
-
+    var url = 'http://localhost:8000/usuarios/crear';
+    postData(url, js);
   }
+  async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer',
+      body: data // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+
+
   return (
     <div style={{ backgroundColor: '#F2F2F2' }}>
       <Header />
