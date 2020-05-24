c61ac34882721f42bf727f6283f8f3e92570a022
Moisesdd <moisesdd@uninorte.edu.co>
2020-05-23 23:11:03 -0500

    put perfil

--- src/components/profile.js
+++ src/components/profile.js
@@ -95,7 +95,7 @@ export default function Profile() {
   }
  /* async function updateData(url = '', data = {}) {
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      method: 'PUT', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
